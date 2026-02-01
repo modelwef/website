ece21341051a6f20bf53f096a0d1999729bfd3c2
AGFH5300 <dxb.avg@gmail.com>
2026-02-01 18:59:13 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import App from "./App.tsx"
+import "./index.css"
 
-createRoot(document.getElementById("root")!).render(<App />);
+import SystemPage from "./routes/system"
+import AuthCallback from "./routes/auth-callback"
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/system" element={<SystemPage />} />
+      <Route path="/auth/callback" element={<AuthCallback />} />
+    </Routes>
+  </BrowserRouter>
+)
